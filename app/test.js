7b48b9e53408ee500a605ca36b2896ac9e56fec3
Mohit Saini <mohitmbdsaini@gmail.com>
2025-08-05 21:47:36 +0530

    updating my code for first time

---
+++ app/test.js
@@ -0,0 +1,109 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { Text, View, Dimensions, StyleSheet, ScrollView } from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+export default function Test() {
+  const [taskData, setTaskData] = useState([]);
+  useFocusEffect(function () {
+    AsyncStorage.getItem("taskData").then(function (data) {
+      if (data) {
+        const taskData = JSON.parse(data);
+        setTaskData(taskData);
+      }
+    });
+  });
+  const daysPerTasks = [];
+  taskData.forEach(function (day) {
+    const totalTasks = day.length;
+    const completedTasks = day.filter(function (task) {
+      return task.isCompleted;
+    }).length;
+    const percentage = totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0;
+    daysPerTasks.push(percentage);
+  });
+
+  const days = [];
+  taskData.forEach(function (_, index) {
+    const dayName = `Day ${index + 1}`;
+    days.push(dayName);
+  });
+
+  const data = {
+    labels: days,
+    datasets: [
+      {
+        data: daysPerTasks,
+      },
+    ],
+  };
+  console.log(days);
+  return (
+    <View>
+      {/* {taskData.map(function (day, index) {
+        return (
+          <View key={index}>
+            <Text>Day {index + 1}</Text>
+            {day.map(function (task, taskIndex) {
+              return (
+                <View key={taskIndex}>
+                  <Text>{task.title}</Text>
+                  {task.isCompleted ? (
+                    <Text>Completed</Text>
+                  ) : (
+                    <Text>Not Completed</Text>
+                  )}
+                </View>
+              );
+            })}
+          </View>
+        );
+      })} */}
+      <View style={styles.graph}>
+        <Text>Bar Chart</Text>
+        {days.length === 0 ? (
+          <Text>No data to display</Text>
+        ) : (
+          <ScrollView horizontal={true}>
+            <BarChart
+              style={styles.graph}
+              data={data}
+              width={Math.max(Dimensions.get("window").width, days.length * 60)}
+              height={220}
+              yAxisLabel=""
+              chartConfig={{
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2,
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "2",
+                  stroke: "#ffa726",
+                },
+              }}
+              verticalLabelRotation={30}
+            />
+          </ScrollView>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  graph: {
+    marginHorizontal: "auto",
+  },
+});
